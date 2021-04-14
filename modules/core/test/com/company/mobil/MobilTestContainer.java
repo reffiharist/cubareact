81e8084efa415b0d992bcec25f8d97fbae915a3b
teddy.siprima <teddy.siprima@gmail.com>
2021-04-14 10:15:09 +0700

    First Commit

---
+++ modules/core/test/com/company/mobil/MobilTestContainer.java
@@ -0,0 +1,57 @@
+package com.company.mobil;
+
+import com.haulmont.cuba.testsupport.TestContainer;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MobilTestContainer extends TestContainer {
+
+    public MobilTestContainer() {
+        super();
+        //noinspection ArraysAsListWithZeroOrOneArgument
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.addon.restapi");
+        appPropertiesFiles = Arrays.asList(
+                // List the files defined in your web.xml
+                // in appPropertiesConfig context parameter of the core module
+                "com/company/mobil/app.properties",
+                // Add this file which is located in CUBA and defines some properties
+                // specifically for test environment. You can replace it with your own
+                // or add another one in the end.
+                "com/company/mobil/test-app.properties");
+        autoConfigureDataSource();
+    }
+
+    
+
+    public static class Common extends MobilTestContainer {
+
+        public static final MobilTestContainer.Common INSTANCE = new MobilTestContainer.Common();
+
+        private static volatile boolean initialized;
+
+        private Common() {
+        }
+
+        @Override
+        public void beforeAll(ExtensionContext extensionContext) throws Exception {
+            if (!initialized) {
+                super.beforeAll(extensionContext);
+                initialized = true;
+            }
+            setupContext();
+        }
+        
+
+        @SuppressWarnings("RedundantThrows")
+        @Override
+        public void afterAll(ExtensionContext extensionContext) throws Exception {
+            cleanupContext();
+            // never stops - do not call super
+        }
+        
+    }
+}
